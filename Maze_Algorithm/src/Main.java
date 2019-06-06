1a6a0c0e4fcd01174e834b51db79f563bf25408d
Na Yunjae_Android <yunjaena@gmail.com>
2019-06-06 21:47:10 +0900

    adjsut comment

--- Maze_Algorithm/src/Main.java
+++ Maze_Algorithm/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private static Mouse mouse;
     private static Maze maze;
     private static final String FILE_NAME = "Maze1.txt";
-    private static final int PRINT_TYPE = Print.TYPE_TWO; //콘솔 출력 이상이 타입을 바꿔주세요
+    private static final int PRINT_TYPE = Print.TYPE_TWO; //콘솔 출력 이상이 타입을 바꿔주세요(TYPE_ONE, TYPE_TWO)
     private static final int MAX_X = 50;
     private static final int MAX_Y = 50;
 
